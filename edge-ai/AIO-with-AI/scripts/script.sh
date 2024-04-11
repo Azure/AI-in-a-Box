e976b6d91414e74599602949524c94d4b8f37458
Andrés Padilla <69865428+AndresPad@users.noreply.github.com>
2024-04-10 20:51:15 -0400

    Update script.sh

--- edge-ai/AIO-with-AI/scripts/script.sh
+++ edge-ai/AIO-with-AI/scripts/script.sh
@@ -44,3 +44,12 @@ export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 #############################
 #Install Helm
 #############################
+echo "#############################"
+echo "Installing Helm"
+echo "#############################"
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+sudo apt-get install apt-transport-https --yes
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
+echo "source <(helm completion bash)" >> /home/$4/.bashrc
