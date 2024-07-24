9927bf359999ad007516846412a5d5704f57361b
AndresPad <anpadill@microsoft.com>
2024-07-24 07:37:27 -0400

    k3

--- edge-ai/AIO-with-AI/scripts/installK3s.sh
+++ edge-ai/AIO-with-AI/scripts/installK3s.sh
@@ -15,15 +15,17 @@ echo $spAppId:$2 | awk '{print substr($1,2); }' >> vars.sh
 echo $spSecret:$3 | awk '{print substr($1,2); }' >> vars.sh
 echo $tenantId:$4 | awk '{print substr($1,2); }' >> vars.sh
 echo $arcK8sClusterName:$5 | awk '{print substr($1,2); }' >> vars.sh
-echo $location:$6 | awk '{print substr($1,2); }' >> vars.sh
-echo $templateBaseUrl:$7 | awk '{print substr($1,2); }' >> vars.sh
+echo $virtualMachineName:$6 | awk '{print substr($1,2); }' >> vars.sh
+echo $location:$7 | awk '{print substr($1,2); }' >> vars.sh
+echo $templateBaseUrl:$8 | awk '{print substr($1,2); }' >> vars.sh
 sed -i '2s/^/export adminUsername=/' vars.sh
 sed -i '3s/^/export appId=/' vars.sh
 sed -i '4s/^/export password=/' vars.sh
 sed -i '5s/^/export tenantId=/' vars.sh
 sed -i '6s/^/export arcK8sClusterName=/' vars.sh
-sed -i '7s/^/export location=/' vars.sh
-sed -i '8s/^/export templateBaseUrl=/' vars.sh
+sed -i '7s/^/export virtualMachineName=/' vars.sh
+sed -i '8s/^/export location=/' vars.sh
+sed -i '9s/^/export templateBaseUrl=/' vars.sh
 
 chmod +x vars.sh
 . ./vars.sh
@@ -69,9 +71,13 @@ sudo -u $adminUsername az login --service-principal --username $appId --password
 
 # Onboard the cluster to Azure Arc and enabling Container Insights using Kubernetes extension
 echo ""
-resourceGroup=$(sudo -u $adminUsername az resource list --query "[?name=='$arcK8sClusterName']".[resourceGroup] --resource-type "Microsoft.Compute/virtualMachines" -o tsv)
-# sudo -u $adminUsername az connectedk8s connect --name $arcK8sClusterName --resource-group $resourceGroup --location $location --kube-config /home/${adminUsername}/.kube/config --tags 'Project=jumpstart_azure_arc_k8s' --correlation-id "d009f5dd-dba8-4ac7-bac9-b54ef3a6671a"
+resourceGroup=$(sudo -u $adminUsername az resource list --query "[?name=='$virtualMachineName']".[resourceGroup] --resource-type "Microsoft.Compute/virtualMachines" -o tsv)
+sudo -u $adminUsername az connectedk8s connect --name $arcK8sClusterName --resource-group $resourceGroup --location $location --kube-config /home/${adminUsername}/.kube/config --tags 'Project=jumpstart_azure_arc_k8s' --correlation-id "d009f5dd-dba8-4ac7-bac9-b54ef3a6671a"
 
 # sudo -u $adminUsername az k8s-extension create -n "azuremonitor-containers" --cluster-name $arcK8sClusterName --resource-group $resourceGroup --cluster-type connectedClusters --extension-type Microsoft.AzureMonitor.Containers
 # sudo -u $adminUsernmae az k8s-extension create -g $resourceGroup -c $arcK8sClusterName -n azureml --cluster-type connectedClusters --extension-type Microsoft.AzureML.Kubernetes --scope cluster \
 #     --config enableTraining=False enableInference=True allowInsecureConnections=True inferenceRouterServiceType=loadBalancer inferenceRouterHA=False autoUpgrade=True installNvidiaDevicePlugin=False installPromOp=False installVolcano=False installDcgmExporter=False --auto-upgrade true --verbose 
+
+
+# sudo -u ArcAdmin az connectedk8s connect --name aiobxcluster --resource-group aibx-aioedgeai-rg --location eastus --kube-config /home/ArcAdmin/.kube/config --tags 'Project=jumpstart_azure_arc_k8s' --correlation-id "d009f5dd-dba8-4ac7-bac9-b54ef3a6671a"
+# sudo -u ArcAdmin az k8s-extension create -n "azuremonitor-containers" --cluster-name aiobxcluster --resource-group aibx-aioedgeai-rg --cluster-type connectedClusters --extension-type Microsoft.AzureMonitor.Containers
