8724065710a34247ce1d846147682f1b417241b2
AndresPad <anpadill@microsoft.com>
2024-08-19 15:08:02 -0400

    Refactor installK3s1.sh script to configure Arc for Kubernetes GitOps and AML Extension

--- edge-ai/AIO-with-AI/scripts/installK3s1.sh
+++ edge-ai/AIO-with-AI/scripts/installK3s1.sh
@@ -161,13 +161,11 @@ az connectedk8s connect --resource-group $rg --name $arcK8sClusterName --locatio
 #Arc for Kubernetes GitOps Extension
 #############################
 echo "Configuring Arc for Kubernetes GitOps"
-az extension add -n k8s-extension --yes
-az extension add -n customlocation --yes
 az extension add -n k8s-configuration --yes
+az extension add -n k8s-extension --yes
 
-
-sudo apt-get update -y
-sudo apt-get upgrade -y
+# sudo apt-get update -y
+# sudo apt-get upgrade -y
 
 # Deploy Extension
 az k8s-extension create \
@@ -180,9 +178,14 @@ az k8s-extension create \
 #############################
 #Arc for Kubernetes Azure Monitor Container Insights Extension
 #############################
-# az config set extension.use_dynamic_install=yes_without_prompt
-# az config set extension.dynamic_install_allow_preview=true
-# az extension add -n k8s-extension --yes
+#https://learn.microsoft.com/en-us/azure/machine-learning/how-to-deploy-kubernetes-extension
+#allowInsecureConnections=True - Allow HTTP communication or not. HTTP communication is not a secure way. If not allowed, HTTPs will be used.
+#InferenceRouterHA=False       - By default, AzureML extension will deploy 3 ingress controller replicas for high availability, which requires at least 3 workers in a cluster. Set this to False if you have less than 3 workers and want to deploy AzureML extension for development and testing only, in this case it will deploy one ingress controller replica only.
+
+az config set extension.use_dynamic_install=yes_without_prompt
+az config set extension.dynamic_install_allow_preview=true
+az extension add -n k8s-extension --yes
+az extension add --name "customlocation" --yes
 
 
 #Deploy Azure Monitor Container Insights Extension
