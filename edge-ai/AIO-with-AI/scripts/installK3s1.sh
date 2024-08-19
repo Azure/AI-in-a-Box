e7933845472e597993a6086f7032ebf2d8727475
AndresPad <anpadill@microsoft.com>
2024-08-19 06:42:38 -0400

    Refactor installK3s1.sh script to improve installation process and remove unnecessary commands

--- edge-ai/AIO-with-AI/scripts/installK3s1.sh
+++ edge-ai/AIO-with-AI/scripts/installK3s1.sh
@@ -157,11 +157,17 @@ az extension add --name connectedk8s --yes
 # Use the az connectedk8s connect command to Arc-enable your Kubernetes cluster and manage it as part of your Azure resource group
 az connectedk8s connect --resource-group $rg --name $arcK8sClusterName --location $location --kube-config /etc/rancher/k3s/k3s.yaml
 
+
+#az k8s-extension create --resource-group $rg --cluster-name $arcK8sClusterName -n "azuremonitor-containers" --cluster-type connectedClusters --extension-type Microsoft.AzureMonitor.Containers
+#az k8s-extension create -g $rg -c $arcK8sClusterName -n "azuremonitor-containers" --cluster-type connectedClusters --extension-type Microsoft.AzureMonitor.Containers 
+
 #############################
 #Arc for Kubernetes GitOps Extension
 #############################
 echo "Configuring Arc for Kubernetes GitOps"
-az extension add -n k8s-configuration --yes
+
+#reorder these switch them
+az extension add -n k8s-configuration --yes 
 az extension add -n k8s-extension --yes
 
 sudo apt-get update -y
@@ -199,28 +205,22 @@ az extension add --name "customlocation" --yes
 #     --cluster-type connectedClusters \
 #     --extension-type Microsoft.AzureMonitor.Containers
 
-az k8s-extension create --resource-group $rg -n azuremonitor-containers --cluster-name $arcK8sClusterName  --cluster-type connectedClusters --extension-type Microsoft.AzureMonitor.Containers --allow-preview true
+# sudo az k8s-extension create --resource-group $rg -n azuremonitor-containers --cluster-name $arcK8sClusterName  --cluster-type connectedClusters --extension-type Microsoft.AzureMonitor.Containers --allow-preview true
 #az k8s-extension create -g aiobx-aioedgeai-rg -c aiobxcluster -n "azuremonitor-containers" --cluster-type connectedClusters --extension-type Microsoft.AzureMonitor.Containers 
 
-#############################
-#Arc for Kubernetes AML Extension
-#############################
-#https://learn.microsoft.com/en-us/azure/machine-learning/how-to-deploy-kubernetes-extension
-#allowInsecureConnections=True - Allow HTTP communication or not. HTTP communication is not a secure way. If not allowed, HTTPs will be used.
-#InferenceRouterHA=False       - By default, AzureML extension will deploy 3 ingress controller replicas for high availability, which requires at least 3 workers in a cluster. Set this to False if you have less than 3 workers and want to deploy AzureML extension for development and testing only, in this case it will deploy one ingress controller replica only.
 
 #az k8s-extension create -g $rg -c $arcK8sClusterName -n "azuremonitor-containers" --cluster-type connectedClusters --extension-type Microsoft.AzureMonitor.Containers 
 
 #az k8s-extension create -g aiobx-aioedgeai-rg -c aiobxcluster -n azureml --cluster-type connectedClusters --extension-type Microsoft.AzureML.Kubernetes --scope cluster --config enableTraining=False enableInference=True allowInsecureConnections=True inferenceRouterServiceType=loadBalancer inferenceRouterHA=False autoUpgrade=True installNvidiaDevicePlugin=False installPromOp=False installVolcano=False installDcgmExporter=False --auto-upgrade true --verbose
 
-# az k8s-extension create \
-#     -g $rg \
-#     -c $arcK8sClusterName \
-#     -n azureml \
-#     --cluster-type connectedClusters \
-#     --extension-type Microsoft.AzureML.Kubernetes \
-#     --scope cluster \
-#     --config enableTraining=False enableInference=True allowInsecureConnections=True inferenceRouterServiceType=loadBalancer inferenceRouterHA=False autoUpgrade=True installNvidiaDevicePlugin=False installPromOp=False installVolcano=False installDcgmExporter=False --auto-upgrade true --verbose 
+az k8s-extension create \
+    -g $rg \
+    -c $arcK8sClusterName \
+    -n azureml \
+    --cluster-type connectedClusters \
+    --extension-type Microsoft.AzureML.Kubernetes \
+    --scope cluster \
+    --config enableTraining=False enableInference=True allowInsecureConnections=True inferenceRouterServiceType=loadBalancer inferenceRouterHA=False autoUpgrade=True installNvidiaDevicePlugin=False installPromOp=False installVolcano=False installDcgmExporter=False --auto-upgrade true --verbose 
 
 
 #############################
