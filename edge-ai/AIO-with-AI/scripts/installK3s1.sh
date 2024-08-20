36b03c2312f5aa5d6d4a21e6317c198c7e24f927
AndresPad <anpadill@microsoft.com>
2024-08-19 21:30:49 -0400

    Refactor installK3s1.sh script to configure Arc for Kubernetes GitOps and AML Extension

--- edge-ai/AIO-with-AI/scripts/installK3s1.sh
+++ edge-ai/AIO-with-AI/scripts/installK3s1.sh
@@ -197,9 +197,6 @@ az extension add --name "customlocation" --yes
 #allowInsecureConnections=True - Allow HTTP communication or not. HTTP communication is not a secure way. If not allowed, HTTPs will be used.
 #InferenceRouterHA=False       - By default, AzureML extension will deploy 3 ingress controller replicas for high availability, which requires at least 3 workers in a cluster. Set this to False if you have less than 3 workers and want to deploy AzureML extension for development and testing only, in this case it will deploy one ingress controller replica only.
 
-
-
-
 az k8s-extension create \
     -g $rg \
     -c $arcK8sClusterName \
@@ -209,15 +206,13 @@ az k8s-extension create \
     --scope cluster \
     --config enableTraining=False enableInference=True allowInsecureConnections=True inferenceRouterServiceType=loadBalancer inferenceRouterHA=False autoUpgrade=True installNvidiaDevicePlugin=False installPromOp=False installVolcano=False installDcgmExporter=False --auto-upgrade true --verbose 
 
-az extension add --upgrade --name azure-iot-ops --allow-preview true --yes
-
 #############################
 #Azure IoT Operations
 #############################
 # Starting off the post deployment steps. The following steps are to deploy Azure IoT Operations components
 # Reference: https://learn.microsoft.com/en-us/azure/iot-operations/deploy-iot-ops/howto-prepare-cluster?tabs=ubuntu#create-a-cluster
-# echo "Deploy IoT Operations CCCCComponents"
-# az extension add --upgrade --name azure-iot-ops --allow-preview true --yes
+echo "Deploy IoT Operations CCCCComponents"
+az extension add --upgrade --name azure-iot-ops --allow-preview true --yes
 
 # echo fs.inotify.max_user_instances=8192 | sudo tee -a /etc/sysctl.conf
 # echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf
