7f60ae53846d56070266b03c65e81a677f616eba
AndresPad <anpadill@microsoft.com>
2024-08-29 12:16:53 -0400

    chore: Remove commented out code and unused variables in installK3s1Vic.sh

--- edge-ai/AIO-with-AI/scripts/installK3s1Vic.sh
+++ edge-ai/AIO-with-AI/scripts/installK3s1Vic.sh
@@ -255,36 +255,36 @@ az k8s-extension create \
 kubectl apply -f https://raw.githubusercontent.com/Azure/arc_jumpstart_drops/main/sample_app/cerebral_genai/deployment/cerebral-ns.yaml
 
 #Create a directory for persistent InfluxDB data
-sudo mkdir /var/lib/influxdb2
-sudo chmod 777 /var/lib/influxdb2
+# sudo mkdir /var/lib/influxdb2
+# sudo chmod 777 /var/lib/influxdb2
 
-#Deploy InfluxDB, Configure InfluxDB, and Deploy the Data Simulator
-kubectl apply -f https://raw.githubusercontent.com/Azure/arc_jumpstart_drops/main/sample_app/cerebral_genai/deployment/influxdb.yaml
-kubectl apply -f https://raw.githubusercontent.com/Azure/arc_jumpstart_drops/main/sample_app/cerebral_genai/deployment/influxdb-setup.yaml
-kubectl apply -f https://raw.githubusercontent.com/Azure/arc_jumpstart_drops/main/sample_app/cerebral_genai/deployment/cerebral-simulator.yaml
+# #Deploy InfluxDB, Configure InfluxDB, and Deploy the Data Simulator
+# kubectl apply -f https://raw.githubusercontent.com/Azure/arc_jumpstart_drops/main/sample_app/cerebral_genai/deployment/influxdb.yaml
+# kubectl apply -f https://raw.githubusercontent.com/Azure/arc_jumpstart_drops/main/sample_app/cerebral_genai/deployment/influxdb-setup.yaml
+# kubectl apply -f https://raw.githubusercontent.com/Azure/arc_jumpstart_drops/main/sample_app/cerebral_genai/deployment/cerebral-simulator.yaml
 
-#Validate the implementation
-kubectl get all -n cerebral
+# #Validate the implementation
+# kubectl get all -n cerebral
 
-#Deploy Redis to store user sessions and conversation history
-kubectl apply -f https://raw.githubusercontent.com/Azure/arc_jumpstart_drops/main/sample_app/cerebral_genai/deployment/redis.yaml
+# #Deploy Redis to store user sessions and conversation history
+# kubectl apply -f https://raw.githubusercontent.com/Azure/arc_jumpstart_drops/main/sample_app/cerebral_genai/deployment/redis.yaml
 
-#Deploy Cerebral Application
-#Download the Cerebral application deployment file
-mkdir -p /home/$adminUsername/cerebral
-wget -p /home/$adminUsername/cerebral https://raw.githubusercontent.com/Azure/arc_jumpstart_drops/main/sample_app/cerebral_genai/deployment/cerebral.yaml
+# #Deploy Cerebral Application
+# #Download the Cerebral application deployment file
+# mkdir -p /home/$adminUsername/cerebral
+# wget -p /home/$adminUsername/cerebral https://raw.githubusercontent.com/Azure/arc_jumpstart_drops/main/sample_app/cerebral_genai/deployment/cerebral.yaml
 
-#Install Dapr runtime on the cluster
-helm repo add dapr https://dapr.github.io/helm-charts/
-helm repo update
-helm upgrade --install dapr dapr/dapr --version=1.11 --namespace dapr-system --create-namespace --wait
+# #Install Dapr runtime on the cluster
+# helm repo add dapr https://dapr.github.io/helm-charts/
+# helm repo update
+# helm upgrade --install dapr dapr/dapr --version=1.11 --namespace dapr-system --create-namespace --wait
 
-#Deploy Azure IoT MQ - Dapr PubSub Component
-kubectl apply -f https://raw.githubusercontent.com/Azure/arc_jumpstart_drops/main/sample_app/cerebral_genai/deployment/rag-on-the-edge/rag-mq-components.yaml 
+# #Deploy Azure IoT MQ - Dapr PubSub Component
+# kubectl apply -f https://raw.githubusercontent.com/Azure/arc_jumpstart_drops/main/sample_app/cerebral_genai/deployment/rag-on-the-edge/rag-mq-components.yaml 
 
-#Deploy RAG on the Edge
-#Deploy tho other components of RAG on the Edge
-kubectl apply -f https://raw.githubusercontent.com/Azure/arc_jumpstart_drops/main/sample_app/cerebral_genai/deployment/rag-on-the-edge/rag-vdb-dapr-workload.yaml
-kubectl apply -f https://raw.githubusercontent.com/Azure/arc_jumpstart_drops/main/sample_app/cerebral_genai/deployment/rag-on-the-edge/rag-interface-dapr-workload.yaml
-kubectl apply -f https://raw.githubusercontent.com/Azure/arc_jumpstart_drops/main/sample_app/cerebral_genai/deployment/rag-on-the-edge/rag-web-workload.yaml
-kubectl apply -f https://raw.githubusercontent.com/Azure/arc_jumpstart_drops/main/sample_app/cerebral_genai/deployment/rag-on-the-edge/rag-llm-dapr-workload.yaml
+# #Deploy RAG on the Edge
+# #Deploy tho other components of RAG on the Edge
+# kubectl apply -f https://raw.githubusercontent.com/Azure/arc_jumpstart_drops/main/sample_app/cerebral_genai/deployment/rag-on-the-edge/rag-vdb-dapr-workload.yaml
+# kubectl apply -f https://raw.githubusercontent.com/Azure/arc_jumpstart_drops/main/sample_app/cerebral_genai/deployment/rag-on-the-edge/rag-interface-dapr-workload.yaml
+# kubectl apply -f https://raw.githubusercontent.com/Azure/arc_jumpstart_drops/main/sample_app/cerebral_genai/deployment/rag-on-the-edge/rag-web-workload.yaml
+# kubectl apply -f https://raw.githubusercontent.com/Azure/arc_jumpstart_drops/main/sample_app/cerebral_genai/deployment/rag-on-the-edge/rag-llm-dapr-workload.yaml
